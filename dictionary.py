e613ebd535f04e87b8a870fc6bd8f54c2de53d38
Cyprian Zdebski <57770826+shipurjan@users.noreply.github.com>
2022-07-13 01:56:51 +0200

    Created a temp dictionary class

---
+++ dictionary.py
@@ -0,0 +1,4 @@
+class dictionary:
+    source = open("json/basic.json")
+    def printSource(self):
+        print(self.source)
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import dictionary
+
 def main():
-    print("lol")
+    obj = dictionary.dictionary()
+    obj.printSource()
 
 if __name__ == '__main__':
     main()
