2e4351159c51e14d3a20b2521cac63b49a688d61
Cyprian Zdebski <57770826+shipurjan@users.noreply.github.com>
2022-07-15 11:57:22 +0200

    Cleaned up.

--- create_language_specific_csvs.py
+++ create_language_specific_csvs.py
@@ -1,11 +1,22 @@
 import csv
-from enum import unique
+import json
+
+def save_json(dictionary, dict_path):
+    with open(dict_path, 'w', encoding='utf-8') as outfile:
+        json.dump(dictionary, outfile, ensure_ascii=False)
+
+def read_json(freq_path):
+    with open(freq_path, 'r', encoding='utf-8') as f_json:
+        freq = json.load(f_json)
+    print("read",freq_path,"...")
+    return freq
 
 def main():
     DELIMITER = chr(31)
-    with open('freq_interslavic.csv', 'r', encoding='utf-8') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=DELIMITER)
-        data = list(csv_reader)
+    DICTIONARY_PATH = 'json/frequency_dict.json'
+    dictionary = read_json(DICTIONARY_PATH)
+    language_codes = ['en', 'ru', 'be', 'uk', 'pl', 'cs', 'sk', 'bg', 'mk', 'sr', 'hr', 'sl']
+    print(dictionary)
 
 
 if __name__ == '__main__':
