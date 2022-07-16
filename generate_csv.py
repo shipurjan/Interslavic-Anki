35f0a97d0b8e78e59ceac2c14419fcc63c7f6e70
Cyprian Zdebski <57770826+shipurjan@users.noreply.github.com>
2022-07-16 13:27:06 +0200

    v0.69

--- generate_csv.py
+++ generate_csv.py
@@ -19,9 +19,9 @@ def read_json(freq_path):
     print("read",freq_path,"...")
     return freq
 
-def HTMLify_word(word):
+def HTMLify_word(word, lang):
     return ''.join((
-        "<div class='word'>",
+        "<div class='",lang," word'>",
             word,
         "</div>"
     ))
@@ -75,9 +75,30 @@ def HTMLify_partOfSpeech(definitions, lang):
     ))
 
 
-def definitions_to_ol(definitions, key):
+def concat_isv(definitions):
+    ol_list = []
     for definition in definitions:
-        ol_list = []
+        li = ''.join((
+            "<li>",
+                "<span class='translations'>",
+                '; '.join(definition['isv']),
+                "</span><span class='conjugations'> (",
+                '; '.join(definition['conjugations']),
+                ")</span>",
+            "</li>"
+        ))
+        ol_list.append(li)
+    return ''.join((
+        "<div class='language isv'>",
+            "<ol>",
+                *ol_list,
+            "</ol>",
+        "</div>"
+    ))
+
+def definitions_to_ol(definitions, key):
+    ol_list = []
+    for definition in definitions:
         li = ''.join((
             "<li>",
                 '; '.join(definition[key]),
@@ -85,7 +106,7 @@ def definitions_to_ol(definitions, key):
         ))
         ol_list.append(li)
     return ''.join((
-        "<div class='",key,"'>",
+        "<div class='language ",key,"'>",
             "<ol>",
                 *ol_list,
             "</ol>",
@@ -97,7 +118,10 @@ def HTMLify_translations(definitions, lang):
     translation_list = []
     for language_code in language_codes:
         if(language_code != lang):
-            translation_list.append(definitions_to_ol(definitions, language_code))
+            if(language_code == 'isv'):
+                translation_list.append(concat_isv(definitions))
+            else:
+                translation_list.append(definitions_to_ol(definitions, language_code))
     if(len(translation_list) == len(language_codes) - 1):
         return translation_list
     return
@@ -110,12 +134,12 @@ def convert_to_flat_list(lang, original_list):
         word = entry[0]
         description = entry[1]
         definitions = description['definitions']
+        sorted_definitions = sorted(definitions, key=lambda d: d['freq_count']) 
         row = [
-            HTMLify_word(word),
+            HTMLify_word(word, lang),
             HTMLify_freq(description['freq']),
-            HTMLify_partOfSpeech(definitions, lang),
-            *HTMLify_translations(definitions, lang),
-            definitions_to_ol(definitions, 'conjugations')
+            HTMLify_partOfSpeech(sorted_definitions, lang),
+            *HTMLify_translations(sorted_definitions, lang),
         ]
         flat_list.append(row)
     return flat_list
@@ -128,24 +152,24 @@ def create_html_page(flat_list):
 
 def main():
     dicts = [
-        ["isv", read_json('json/build/final_frequency_dict.json')],
-        ["be", read_json('json/build/final_frequency_dict_be.json')],
-        ["bg", read_json('json/build/final_frequency_dict_bg.json')],
-        ["cs", read_json('json/build/final_frequency_dict_cs.json')],
-        ["en", read_json('json/build/final_frequency_dict_en.json')],
-        ["hr", read_json('json/build/final_frequency_dict_hr.json')],
-        ["mk", read_json('json/build/final_frequency_dict_mk.json')],
+        # ["isv", read_json('json/build/final_frequency_dict.json')],
+        # ["be", read_json('json/build/final_frequency_dict_be.json')],
+        # ["bg", read_json('json/build/final_frequency_dict_bg.json')],
+        # ["cs", read_json('json/build/final_frequency_dict_cs.json')],
+        # ["en", read_json('json/build/final_frequency_dict_en.json')],
+        # ["hr", read_json('json/build/final_frequency_dict_hr.json')],
+        # ["mk", read_json('json/build/final_frequency_dict_mk.json')],
         ["pl", read_json('json/build/final_frequency_dict_pl.json')],
-        ["ru", read_json('json/build/final_frequency_dict_ru.json')],
-        ["sk", read_json('json/build/final_frequency_dict_sk.json')],
-        ["sl", read_json('json/build/final_frequency_dict_sl.json')],
-        ["sr", read_json('json/build/final_frequency_dict_sr.json')],
-        ["uk", read_json('json/build/final_frequency_dict_uk.json')]
+        # ["ru", read_json('json/build/final_frequency_dict_ru.json')],
+        # ["sk", read_json('json/build/final_frequency_dict_sk.json')],
+        # ["sl", read_json('json/build/final_frequency_dict_sl.json')],
+        # ["sr", read_json('json/build/final_frequency_dict_sr.json')],
+        # ["uk", read_json('json/build/final_frequency_dict_uk.json')]
     ]
     for dict in dicts:
         frequency_list = convert_to_flat_list(dict[0], dict[1]["frequency_order"])
         rare_list = convert_to_flat_list(dict[0], dict[1]["random_order"])
-        complete_list = frequency_list + rare_list
+        complete_list = frequency_list + rare_list 
         save_csv('csv/'+dict[0]+'.csv', complete_list)
 
 if __name__ == '__main__':
